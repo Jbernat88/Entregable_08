0a79e9e3aa43b9b65f75ca6d55d373a30c4f74d6
Jbernat88 <91455587+Jbernat88@users.noreply.github.com>
2022-03-17 14:49:44 +0100

    ddddddddddd

--- Assets/Scrpit/AudioManager.cs
+++ Assets/Scrpit/AudioManager.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-
+    private int actualSong;
     public AudioClip[] music;
+    private AudioSource audio;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audio = GetComponent<AudioSource>();
+        audio.PlayOneShot(music[actualSong]);
     }
 
     // Update is called once per frame
@@ -17,4 +19,43 @@ public class AudioManager : MonoBehaviour
     {
         
     }
+
+    public void PauseButtom()
+    {
+        audio.Pause();
+    }
+
+    public void PlayButtom()
+    {
+        audio.UnPause();
+    }
+
+    public void NextButtom()
+    {
+        actualSong++;
+        audio.PlayOneShot(music[actualSong],1f);
+
+        if (actualSong >= music.Length)
+        {
+            actualSong = 0;
+        }
+
+        audio.Stop();
+    }
+
+    public void BackButtom()
+    {
+        actualSong--;
+        audio.PlayOneShot(music[actualSong]);
+
+        if (actualSong <=0)
+        {
+            actualSong = music.Length -1;
+        }
+
+        audio.Stop();
+    }
+
+
+
 }
