1dc4530bc42fa19031bdfb5e877cc6a73beb367b
Joan Bernat <joanbernatmunyoz@gmail.com>
2022-03-18 21:32:38 +0100

    ffffffffffffff

--- Assets/Scrpit/AudioManager.cs
+++ Assets/Scrpit/AudioManager.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+ 
 
 public class AudioManager : MonoBehaviour
 {
-    private int actualSong;
+    private int actualSong;//Array (lista de canciones)
     public AudioClip[] music;
     private AudioSource audio;
+    public TextMeshProUGUI songTitle;//Variable para acceder al titulo
+    public string[] nameSong;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,23 +23,25 @@ public class AudioManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        songTitle.text = nameSong[actualSong]; //Canvia el titulo de la cancion dependio de la canción que suena
     }
 
+    //Funcion para pausar la canción
     public void PauseButtom()
     {
         audio.Pause();
     }
 
+    //Función para reaunudar la canción
     public void PlayButtom()
     {
         audio.UnPause();
     }
 
+    //Funcion para Pasar a la siguiente canción de la lista
     public void NextButtom()
     {
         actualSong++;
-        audio.PlayOneShot(music[actualSong],1f);
 
         if (actualSong >= music.Length)
         {
@@ -41,21 +49,32 @@ public class AudioManager : MonoBehaviour
         }
 
         audio.Stop();
+        audio.PlayOneShot(music[actualSong], 1f);
+
     }
 
+    //Función para volver a la anterior canción de la lista
     public void BackButtom()
     {
         actualSong--;
-        audio.PlayOneShot(music[actualSong]);
-
+   
         if (actualSong <=0)
         {
             actualSong = music.Length -1;
         }
 
         audio.Stop();
+        audio.PlayOneShot(music[actualSong]);
     }
 
+    //Función para Reproducir una canción random de la lista
+    public void ShuffleButtom()
+    {
+        
+     audio.Stop();
+     actualSong = Random.Range(0, music.Length);
+     audio.PlayOneShot(music[actualSong]);
 
+    }
 
 }
